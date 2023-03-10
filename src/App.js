684286a9ae63f0b1f691ec5237cc961666f22d0b
Vineet <vineet.22566@sscbs.du.ac.in>
2023-03-10 19:34:34 +0530

    homepage tested

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/nancy/Homepage';
 import Locationpage from './components/vinu/Locationpage';
 import Maps from './components/vinu/Maps';
 
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Locationpage/>}/>
+          <Route path='/' element={<Homepage/>}/>
           
         </Routes>
       </Router>
