15546bcbc005f91d87aa2fd39085287b014e35ef
Vineet <vineet.22566@sscbs.du.ac.in>
2023-03-10 19:32:39 +0530

    tested login page

---
+++ src/components/muskan/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            LOGIN PAGE
+            Hi I am muskan
+            <h1>HOLA</h1>
+        </div>
+    );
+}
+
+export default Login;
